0869c15422e2971187dd70cd44a4c9f1c32478cc
sergeytutushkin <sergeytutushkin@gmail.com>
2020-11-19 19:49:52 +0300

    Created movie details layout

--- app/src/main/java/dev/tutushkin/aagfundamentals2020/MainActivity.kt
+++ app/src/main/java/dev/tutushkin/aagfundamentals2020/MainActivity.kt
@@ -1,11 +1,21 @@
 package dev.tutushkin.aagfundamentals2020
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val textView: TextView = findViewById(R.id.tvHelloWorld)
+        textView.setOnClickListener { moveToDetailsScreen() }
+    }
+
+    private fun moveToDetailsScreen() {
+        val intent = Intent(this, MovieDetailsActivity::class.java)
+        startActivity(intent)
     }
 }
