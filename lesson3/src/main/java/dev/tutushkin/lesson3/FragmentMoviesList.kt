500df5b04859a39f2014aadbaf43947dcd7f0e5c
sergeytutushkin <sergeytutushkin@gmail.com>
2020-11-30 17:35:02 +0300

    Created FragmentMoviesList.kt and inflated fragment_movies_list.xml in it

---
+++ lesson3/src/main/java/dev/tutushkin/lesson3/FragmentMoviesList.kt
@@ -0,0 +1,15 @@
+package dev.tutushkin.lesson3
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
+class FragmentMoviesList : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? = inflater.inflate(R.layout.fragment_movies_list, container, false)
+}
