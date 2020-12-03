4d72b4778505aa1e171483c042e231c281ec063f
sergeytutushkin <sergeytutushkin@gmail.com>
2020-12-04 01:13:52 +0300

    Cleared Kotlin code

--- lesson3/src/main/java/dev/tutushkin/lesson3/FragmentMoviesDetails.kt
+++ lesson3/src/main/java/dev/tutushkin/lesson3/FragmentMoviesDetails.kt
@@ -1,26 +1,17 @@
 package dev.tutushkin.lesson3
 
 import android.os.Bundle
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 
-class FragmentMoviesDetails : Fragment() {
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? = inflater.inflate(R.layout.fragment_movies_details, container, false)
+class FragmentMoviesDetails : Fragment(R.layout.fragment_movies_details) {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
         view.findViewById<TextView>(R.id.back_text).setOnClickListener {
-            activity?.let {
-                it.supportFragmentManager.popBackStack()
-            }
+            requireActivity().supportFragmentManager.popBackStack()
         }
     }
 }
--- lesson3/src/main/java/dev/tutushkin/lesson3/FragmentMoviesList.kt
+++ lesson3/src/main/java/dev/tutushkin/lesson3/FragmentMoviesList.kt
@@ -1,31 +1,21 @@
 package dev.tutushkin.lesson3
 
 import android.os.Bundle
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
 import androidx.cardview.widget.CardView
 import androidx.fragment.app.Fragment
 
-class FragmentMoviesList : Fragment() {
-
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? = inflater.inflate(R.layout.fragment_movies_list, container, false)
+class FragmentMoviesList() : Fragment(R.layout.fragment_movies_list) {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
         view.findViewById<CardView>(R.id.movies_list_movie_card).apply {
             setOnClickListener {
-                activity?.let {
-                    it.supportFragmentManager.beginTransaction()
-                        .addToBackStack(null)
-                        .replace(R.id.main_container, FragmentMoviesDetails())
-                        .commit()
-                }
+                requireActivity().supportFragmentManager.beginTransaction()
+                    .addToBackStack(null)
+                    .replace(R.id.main_container, FragmentMoviesDetails())
+                    .commit()
             }
         }
     }
