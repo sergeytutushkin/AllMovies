a9374b4cfafbbbfca7f1e81ad0d11213f51a53ed
sergeytutushkin <sergeytutushkin@gmail.com>
2020-12-07 16:11:55 +0300

    Init Lesson 4

---
+++ lesson4/src/main/java/dev/tutushkin/lesson4/FragmentMoviesDetails.kt
@@ -0,0 +1,17 @@
+package dev.tutushkin.lesson4
+
+import android.os.Bundle
+import android.view.View
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+
+class FragmentMoviesDetails : Fragment(R.layout.fragment_movies_details) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        view.findViewById<TextView>(R.id.back_text).setOnClickListener {
+            requireActivity().supportFragmentManager.popBackStack()
+        }
+    }
+}
