7903ac1a396b38f3c5dc5b825cb9ef5a0d48c010
sergeytutushkin <sergeytutushkin@gmail.com>
2020-12-15 00:01:56 +0300

    Created ViewHolder and ViewAdapter for RecyclerView in movies list

--- lesson4/src/main/java/dev/tutushkin/lesson4/FragmentMoviesList.kt
+++ lesson4/src/main/java/dev/tutushkin/lesson4/FragmentMoviesList.kt
@@ -2,22 +2,30 @@ package dev.tutushkin.lesson4
 
 import android.os.Bundle
 import android.view.View
-import androidx.cardview.widget.CardView
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class FragmentMoviesList() : Fragment(R.layout.fragment_movies_list) {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        view.findViewById<CardView>(R.id.movies_list_movie_card).apply {
-            setOnClickListener {
+        val recycler = view.findViewById<RecyclerView>(R.id.movie_list_recycler)
+        recycler.layoutManager = GridLayoutManager(requireContext(), 2)
+
+        val listener = object : MoviesListClickListener {
+            override fun onItemClick(movie: Movie) {
                 requireActivity().supportFragmentManager.beginTransaction()
                     .addToBackStack(null)
                     .replace(R.id.main_container, FragmentMoviesDetails())
                     .commit()
             }
         }
+        val movies = MoviesDataSource().generateMovies()
+        val adapter = MovieAdapter(movies, listener)
+
+        recycler.adapter = adapter
     }
 
 }
